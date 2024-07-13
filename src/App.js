1936c4847bf141ad60f455059a8edf1b08faaeb9
SiddharthSinghRawat01 <siddharthsinghrawat01@gmail.com>
2024-07-13 15:31:49 +0530

    Css fixed

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
   const [time, setTime] = useState(0);
   const [running, setRunning] = useState(false);
 
-  useEffect(() => { //use to create side effect
-    let intervel;
+  useEffect(() => {
+    let interval;
     if (running) {
-      intervel = setInterval(() => {
+      interval = setInterval(() => {
         setTime((prevTime) => prevTime + 10);
       }, 10);
     } else if (!running) {
-      clearInterval(intervel);
+      clearInterval(interval);
     }
-    return () => clearInterval(intervel);
-  }, [running])
+    return () => clearInterval(interval);
+  }, [running]);
 
   return (
-    <>
-      <h1>StopWatch</h1>
-      <div>
+    <div className='max-w-md flex flex-col items-center justify-center py-8'>
+      <h1 className='text-2xl font-semibold'>StopWatch</h1>
+      <div className='text-xl font-semibold py-4'>
         <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
         <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
         <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
       </div>
-      <div>
-        {!running ? 
-        ( <button onClick={() => setRunning(true)}>start</button>) :
-        (<button onClick={() => setRunning(false)}>stop</button>)
+      <div className='w-1/3 max-w-sm flex flex-row justify-evenly'>
+        {!running ?
+          (<button
+            className='border rounded-lg py-1 px-3'
+            onClick={() => setRunning(true)}
+          >
+            Start
+          </button>) :
+          (<button
+            className='border rounded-lg py-1 px-3.5'
+            onClick={() => setRunning(false)}
+          >
+            Stop
+          </button>)
         }
-        <button onClick={() => setTime(0)}>Reset</button>
+        <button
+          className='border rounded-lg py-1 px-2.5'
+          onClick={() => setTime(0)}
+        >
+          Reset
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
